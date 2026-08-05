636130d60cec8573e5d7024c89bafbe5523a857f
SH <haoshen_ai@163.com>
2026-08-05 11:27:53 +0000

    init

--- script/run_launch_va_server_sync.sh
+++ script/run_launch_va_server_sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -x
+set -euo pipefail
 
 umask 007
  
@@ -10,11 +10,9 @@ PORT=${PORT:-"1106"}
 LOG_RANK=${LOG_RANK:-"0"}
 TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE:-"http://localhost:29510"}
 CONFIG_NAME=${CONFIG_NAME:-"robotwin"}
+SAVE_ROOT=${SAVE_ROOT:-"./output"}  # self set the save root path, default is ./output
+WAN22_PRETRAINED_MODEL_NAME_OR_PATH=${WAN22_PRETRAINED_MODEL_NAME_OR_PATH:-"./models/pretrained/lingbot-va-base"}
 
-overrides=""
-if [ $# -ne 0 ]; then
-    overrides="$*"
-fi
 
 ## node setting
 num_gpu=${NGPU}
@@ -22,13 +20,20 @@ master_port=${MASTER_PORT}
 log_rank=${LOG_RANK}
 torchft_lighthouse=${TORCHFT_LIGHTHOUSE}
 config_name=${CONFIG_NAME}
+save_root=${SAVE_ROOT}
+wan22_pretrained_model_name_or_path=${WAN22_PRETRAINED_MODEL_NAME_OR_PATH}
+
 
 ## cmd setting
 export TOKENIZERS_PARALLELISM=false
-PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" TORCHFT_LIGHTHOUSE=${torchft_lighthouse} \
+PYTORCH_ALLOC_CONF="expandable_segments:True" TORCHFT_LIGHTHOUSE=${torchft_lighthouse} \
 python -m torch.distributed.run \
     --nproc_per_node=${num_gpu} \
     --local-ranks-filter=${log_rank} \
     --master_port ${master_port} \
     --tee 3 \
-    -m wan_va.wan_va_server --config-name ${config_name} $overrides
+    -m wan_va.wan_va_server \
+    --config-name ${config_name} \
+    --save-root ${save_root} \
+    --wan22-pretrained-model-name-or-path "${wan22_pretrained_model_name_or_path}"
+
